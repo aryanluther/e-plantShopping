a8d3711fa38baca39119259a13e8a8fb8e007f1f
aryanluther <aryanluthra2014@gmail.com>
2024-12-20 11:48:46 -0500

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
-
 export default defineConfig({
   base: "/shoppingreact",
   plugins: [react()],
-  optimizeDeps: {
-    include: ['linked-dep'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/linked-dep/, /node_modules/],
-    },
-  },
 })
+
